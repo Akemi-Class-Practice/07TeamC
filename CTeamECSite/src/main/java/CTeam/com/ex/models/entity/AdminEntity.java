2044c9a4a9d70689debba112ffd2a1bce6afe31a
li2543868527 <lhc19971997@gmail.com>
2023-08-29 16:57:09 +0900

    フレームのベーシック構築です。

---
+++ CTeamECSite/src/main/java/CTeam/com/ex/models/entity/AdminEntity.java
@@ -0,0 +1,97 @@
+package CTeam.com.ex.models.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "admin")
+public class AdminEntity {
+	
+	@Id
+	@Column(name = "admin_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long adminId;
+	
+	@Column(name = "admin_name")
+	private String adminName;
+	
+	@Column(name = "admin_email")
+	private String adminEmail;
+	
+	@Column(name = "admin_password")
+	private String adminPassword;
+	
+	@Column(name = "delete_flg")
+	private int deleteFlg;
+	
+	@Column(name = "register_date")
+	private LocalDate registerDate;
+
+	public AdminEntity(long adminId, String adminName, String adminEmail, String adminPassword, int deleteFlg,
+			LocalDate registerDate) {
+		this.adminId = adminId;
+		this.adminName = adminName;
+		this.adminEmail = adminEmail;
+		this.adminPassword = adminPassword;
+		this.deleteFlg = deleteFlg;
+		this.registerDate = registerDate;
+	}
+
+	public long getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(long adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getAdminName() {
+		return adminName;
+	}
+
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+
+	public String getAdminEmail() {
+		return adminEmail;
+	}
+
+	public void setAdminEmail(String adminEmail) {
+		this.adminEmail = adminEmail;
+	}
+
+	public String getAdminPassword() {
+		return adminPassword;
+	}
+
+	public void setAdminPassword(String adminPassword) {
+		this.adminPassword = adminPassword;
+	}
+
+	public int getDeleteFlg() {
+		return deleteFlg;
+	}
+
+	public void setDeleteFlg(int deleteFlg) {
+		this.deleteFlg = deleteFlg;
+	}
+
+	public LocalDate getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(LocalDate registerDate) {
+		this.registerDate = registerDate;
+	}
+	
+	
+	
+	
+}
---
+++ CTeamECSite/src/main/java/CTeam/com/ex/models/entity/Transaction_ItemEntity.java
@@ -0,0 +1,56 @@
+package CTeam.com.ex.models.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "transaction_item")
+public class Transaction_ItemEntity {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name = "lesson_id")
+	private long lessonId;
+	
+	@Column(name = "transaction_id")
+	private long transactionId;
+
+	public Transaction_ItemEntity(long id, long lessonId, long transactionId) {
+		this.id = id;
+		this.lessonId = lessonId;
+		this.transactionId = transactionId;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getLessonId() {
+		return lessonId;
+	}
+
+	public void setLessonId(long lessonId) {
+		this.lessonId = lessonId;
+	}
+
+	public long getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(long transactionId) {
+		this.transactionId = transactionId;
+	}
+	
+	
+}
