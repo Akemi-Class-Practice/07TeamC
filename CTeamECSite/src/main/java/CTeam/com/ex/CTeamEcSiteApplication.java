f23c85c24e59eb81a1796b4a3c937aed4e4fa378
li2543868527 <lhc19971997@gmail.com>
2023-08-31 13:17:29 +0900

    还原了ignore的正常版本

--- CTeamECSite/src/main/java/CTeam/com/ex/CTeamEcSiteApplication.java
+++ CTeamECSite/src/main/java/CTeam/com/ex/CTeamEcSiteApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-secret.properties")
 public class CTeamEcSiteApplication {
 
 	public static void main(String[] args) {
