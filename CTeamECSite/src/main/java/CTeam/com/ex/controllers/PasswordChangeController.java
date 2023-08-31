1104d47788935adba0aaa5c804d0d34257b99dd2
li2543868527 <lhc19971997@gmail.com>
2023-08-31 21:06:59 +0900

    captcha完全实装代码

--- CTeamECSite/src/main/java/CTeam/com/ex/controllers/PasswordChangeController.java
+++ CTeamECSite/src/main/java/CTeam/com/ex/controllers/PasswordChangeController.java
@@ -12,55 +12,75 @@ import CTeam.com.ex.models.entity.EmailVerificationEntity;
 import CTeam.com.ex.services.EmailVerificationService;
 import jakarta.servlet.http.HttpSession;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.ByteArrayOutputStream;
+
 @Controller
 public class PasswordChangeController {
-	
+
 	@Autowired
 	private HttpSession session;
-	
+
 	@Autowired
 	private EmailVerificationService emailVerificationService;
-	
+
+	// 提供一个端点为前端生成CAPTCHA图片
+	@GetMapping("/captcha")
+	public ResponseEntity<byte[]> getCaptchaImage() throws Exception {
+		BufferedImage captchaImage = emailVerificationService.generateCaptchaImage();
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ImageIO.write(captchaImage, "png", baos);
+		byte[] imageBytes = baos.toByteArray();
+
+		return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(imageBytes);
+	}
+
 	@GetMapping("/password/reset")
 	public String getPasswordResetPage() {
-		
+
 		return "password_reset.html";
 	}
-	
+
 	@PostMapping("/password/reset/sencode")
 	@ResponseBody
-	public String sendVerificationCode(@RequestParam String email, Model model) {
-	    try {
-	        EmailVerificationEntity verificationEntity = emailVerificationService.generateVerificationCode(email);
-	        emailVerificationService.sendVerificationCodeEmail(email, verificationEntity.getVerificationCode());
-	        session.setAttribute("email", email); // 保存邮箱到会话
-	        return "success";
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        return "error";
-	    }
+	public String sendVerificationCode(@RequestParam String email, @RequestParam String captchaValue, // 用户输入的CAPTCHA值
+			Model model) {
+		if (!emailVerificationService.verifyCaptcha(captchaValue)) {
+			// CAPTCHA验证失败
+			return "captcha_error";
+		}
+
+		try {
+			EmailVerificationEntity verificationEntity = emailVerificationService.generateVerificationCode(email);
+			emailVerificationService.sendVerificationCodeEmail(email, verificationEntity.getVerificationCode());
+			session.setAttribute("email", email); // 保存邮箱到会话
+			return "success";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
 	}
-	
+
 	@PostMapping("/password/reset/verify")
 	public String verifyCode(@RequestParam String verificationCode, Model model) {
-	    String email = (String) session.getAttribute("email"); // 从会话中获取之前保存的邮箱
-	    if (email != null && emailVerificationService.verifyCode(email, verificationCode)) {
-	        // 验证成功，可以重定向到成功页面或其他操作
-	        return "redirect:/password/change"; 
-	    } else {
-	        // 验证失败，重定向到失败页面
-	        return "redirect:/error"; 
-	    }
+		String email = (String) session.getAttribute("email"); // 从会话中获取之前保存的邮箱
+		if (email != null && emailVerificationService.verifyCode(email, verificationCode)) {
+			// 验证成功，可以重定向到成功页面或其他操作
+			return "redirect:/password/change";
+		} else {
+			// 验证失败，重定向到失败页面
+			return "redirect:/error";
+		}
 	}
-	
 
 	@GetMapping("/password/change")
 	public String getPasswordChangePage() {
-		
+
 		return "password_change.html";
 	}
-	
-	
-	
-	
+
 }
--- CTeamECSite/src/main/java/CTeam/com/ex/services/EmailVerificationService.java
+++ CTeamECSite/src/main/java/CTeam/com/ex/services/EmailVerificationService.java
@@ -7,8 +7,16 @@ import org.springframework.stereotype.Service;
 
 import CTeam.com.ex.models.dao.EmailVerificationDao;
 import CTeam.com.ex.models.entity.EmailVerificationEntity;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpSession;
 
+import com.google.code.kaptcha.Producer; 
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+
+import java.awt.image.BufferedImage;
 import java.sql.Timestamp;
+import java.util.Properties;
 
 @Service
 public class EmailVerificationService {
@@ -22,6 +30,11 @@ public class EmailVerificationService {
     
     @Autowired
     private JavaMailSender javaMailSender;
+    
+    @Autowired
+    private HttpSession httpSession;
+
+    private Producer captchaProducer;
 
     public EmailVerificationEntity generateVerificationCode(String email) {
         // 
@@ -65,5 +78,25 @@ public class EmailVerificationService {
     private String generateRandomCode() {
     	VerificationCodeGenerator a = new VerificationCodeGenerator();
 		return a.generateVerificationCode();
+    }
+    
+    @PostConstruct
+    public void init() {
+        DefaultKaptcha kaptcha = new DefaultKaptcha();
+        // 这里可以设置Kaptcha的属性，如图片大小、字符长度等
+        // 示例设置：
+        kaptcha.setConfig(new Config(new Properties()));
+        this.captchaProducer = kaptcha;
+    }
+
+    public BufferedImage generateCaptchaImage() {
+        String captchaText = captchaProducer.createText();
+        httpSession.setAttribute("captcha", captchaText);
+        return captchaProducer.createImage(captchaText);
+    }
+
+    public boolean verifyCaptcha(String userInput) {
+        String storedCaptcha = (String) httpSession.getAttribute("captcha");
+        return storedCaptcha != null && storedCaptcha.equals(userInput);
     }
 }
